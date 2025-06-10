b6dd4f63a8a707fbca34cef9d33b80bd833fa167
Shivansh Bakshi <work@shivanshbakshi.dev>
2025-06-10 17:48:45 +0800

    fix: vllm provider details

--- apps/opik-frontend/src/hooks/useVllmAIProviderData.ts
+++ apps/opik-frontend/src/hooks/useVllmAIProviderData.ts
@@ -1,14 +1,13 @@
-import { CLOUD_PROVIDER_OPTION_TYPE, PROVIDERS } from "@/constants/providers";
 import {
   PROVIDER_MODEL_TYPE,
   PROVIDER_MODELS_TYPE,
   PROVIDER_TYPE,
+  ProviderKey,
   ProviderKeyWithAPIKey,
 } from "@/types/providers";
 import useProviderKeys from "@/api/provider-keys/useProviderKeys";
 import useAppStore from "@/store/AppStore";
 import { useCallback, useMemo } from "react";
-import isEmpty from "lodash/isEmpty";
 
 const useVllmAIProviderData = () => {
   const workspaceName = useAppStore((state) => state.activeWorkspaceName);
@@ -16,17 +15,28 @@ const useVllmAIProviderData = () => {
     workspaceName,
   });
 
-  const getVllmAIProviderData = useCallback((provider: PROVIDER_TYPE) => {
+  const getVllmAIProviderData = useCallback(() => {
     let retVal: ProviderKeyWithAPIKey | undefined = undefined;
-    const config = PROVIDERS[provider] as CLOUD_PROVIDER_OPTION_TYPE;
+    if (data) {
+      const vllmConfig = data.content.find(
+        (providerKey) => providerKey.provider === PROVIDER_TYPE.VLLM,
+      );
 
-    if (config) {
-      const providerData = {};
-      retVal = isEmpty(providerData) ? undefined : providerData;
+      if (vllmConfig) {
+        // Not using spread, we don't need all the properties
+        const _vllmConfig = vllmConfig as (ProviderKey & { base_url: string })
+        retVal = {
+          id: _vllmConfig.id,
+          keyName: _vllmConfig.keyName,
+          provider: _vllmConfig.provider,
+          created_at: _vllmConfig.created_at,
+          url: _vllmConfig.base_url,
+          apiKey: "*****",
+        };
+      }
     }
-
     return retVal;
-  }, []);
+  }, [data]);
 
   const getVllmServerModels = useCallback((baseUrl: string) => {
     const retVal: string[] = [];
@@ -37,29 +47,24 @@ const useVllmAIProviderData = () => {
   const vllmModels = useMemo(() => {
     const retVal: Partial<PROVIDER_MODELS_TYPE> = {};
 
-    console.log(data);
-    if (data) {
-      data.content.forEach((providerKey) => {
-        if (providerKey.provider === PROVIDER_TYPE.VLLM) {
-          const providerData = getVllmAIProviderData(providerKey.provider);
-          console.log(providerData);
-          if (providerData?.url) {
-            const models = getVllmServerModels(providerData.url);
-            retVal[providerKey.provider] = models.map((m) => ({
-              value: m.trim() as PROVIDER_MODEL_TYPE,
-              label: m.trim(),
-            }));
-          }
-        }
-      });
+    const providerData = getVllmAIProviderData();
+
+    if (providerData?.url) {
+      const models = getVllmServerModels(providerData.url);
+      retVal[providerData.provider] = models.map((m) => ({
+        value: m.trim() as PROVIDER_MODEL_TYPE,
+        label: m.trim(),
+      }));
     }
+
     console.log(retVal);
     return retVal;
-  }, [data, getVllmAIProviderData, getVllmServerModels]);
+  }, [getVllmAIProviderData, getVllmServerModels]);
 
   return {
     vllmModels,
     getVllmAIProviderData,
+    getVllmServerModels,
   };
 };
 
