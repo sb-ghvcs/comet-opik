cf1ec19d30ce3bb3651188fd8d7cf9aee021d828
Shivansh Bakshi <work@shivanshbakshi.dev>
2025-06-10 09:06:53 +0000

    debug vllm ai provider methods

--- apps/opik-frontend/src/hooks/useVllmAIProviderData.ts
+++ apps/opik-frontend/src/hooks/useVllmAIProviderData.ts
@@ -1,34 +1,65 @@
-import { useQuery } from "@tanstack/react-query";
-
-import { PROVIDERS_OPTIONS } from "@/constants/providers";
-import { PROVIDER_MODELS_TYPE, PROVIDER_TYPE } from "@/types/providers";
-import { PROVIDERS_KEYS_KEY } from "@/api/api";
+import { CLOUD_PROVIDER_OPTION_TYPE, PROVIDERS } from "@/constants/providers";
+import {
+  PROVIDER_MODEL_TYPE,
+  PROVIDER_MODELS_TYPE,
+  PROVIDER_TYPE,
+  ProviderKeyWithAPIKey,
+} from "@/types/providers";
+import useProviderKeys from "@/api/provider-keys/useProviderKeys";
+import useAppStore from "@/store/AppStore";
+import { useCallback, useMemo } from "react";
+import isEmpty from "lodash/isEmpty";
 
 const useVllmAIProviderData = () => {
-  //   const queryClient = useQueryClient();
+  const workspaceName = useAppStore((state) => state.activeWorkspaceName);
+  const { data } = useProviderKeys({
+    workspaceName,
+  });
 
-  const { data: vllmModels } = useQuery({
-    queryKey: [PROVIDERS_KEYS_KEY],
-    queryFn: () => {
-      const retVal: Partial<PROVIDER_MODELS_TYPE> = {};
+  const getVllmAIProviderData = useCallback((provider: PROVIDER_TYPE) => {
+    let retVal: ProviderKeyWithAPIKey | undefined = undefined;
+    const config = PROVIDERS[provider] as CLOUD_PROVIDER_OPTION_TYPE;
 
-      PROVIDERS_OPTIONS.forEach((option) => {
-        if (option.value === PROVIDER_TYPE.VLLM) {
-          //   retVal[option.value] = option.models.map((m) => ({
-          //       value: m.trim() as PROVIDER_MODEL_TYPE,
-          //       label: m.trim(),
-          //     }));
-          // TODO: Build the list of models from the vllm server
-          //   }
+    if (config) {
+      const providerData = {};
+      retVal = isEmpty(providerData) ? undefined : providerData;
+    }
+
+    return retVal;
+  }, []);
+
+  const getVllmServerModels = useCallback((baseUrl: string) => {
+    const retVal: string[] = [];
+
+    return retVal;
+  }, []);
+
+  const vllmModels = useMemo(() => {
+    const retVal: Partial<PROVIDER_MODELS_TYPE> = {};
+
+    console.log(data);
+    if (data) {
+      data.content.forEach((providerKey) => {
+        if (providerKey.provider === PROVIDER_TYPE.VLLM) {
+          const providerData = getVllmAIProviderData(providerKey.provider);
+          console.log(providerData);
+          if (providerData?.url) {
+            const models = getVllmServerModels(providerData.url);
+            retVal[providerKey.provider] = models.map((m) => ({
+              value: m.trim() as PROVIDER_MODEL_TYPE,
+              label: m.trim(),
+            }));
+          }
         }
       });
-
-      return retVal;
-    },
-  });
+    }
+    console.log(retVal);
+    return retVal;
+  }, [data, getVllmAIProviderData, getVllmServerModels]);
 
   return {
     vllmModels,
+    getVllmAIProviderData,
   };
 };
 
